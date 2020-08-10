b14a55dd6b5e8a790407a1d7379a706b0c9af14b
ashwiniraokarai <ashwini@biggerpockets.com>
2020-08-10 01:26:30 -0600

    provide a way for users to create articles thro the web browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    # Wrapping a blank (unsaved) object in @article instance var
+    # just to get around the nil error for @article on form.
+    # If you tried this in the console, the resulting object look like so:
+    # => #<Article id: nil, title: nil, body: nil, created_at: nil, updated_at: nil>
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: params[:article][:title], body: params[:article][:body] )
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
