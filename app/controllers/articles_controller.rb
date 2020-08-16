d4f47a35cb76922c2c2d0b60d0e6bac3ede3bede
ashwiniraokarai <ashwini@biggerpockets.com>
2020-08-16 02:00:32 -0600

    flash confirmation message in views using flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,16 +19,22 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.create(article_params)
+    flash.notice = "Your article '#{@article.title}' has been saved!"
     # popular alternative:
       # @article = Article.new(article_params)
-      # @article.save
-    # finally call the show page
+      # if @article.save then set a success message flash.notice = "Save successful" and redirect_to show page
+      # else set set an alert for the user using flash.alert = "Save unsuccessful"
+      # ..and render new page again to allow for changes and resubmission
+      # inspect further details using active model's errors method @article.errors.full_messages
+
+    # call the show page
     redirect_to article_path(@article.id)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Your article '#{@article.title}' has been deleted."
     redirect_to articles_path
   end
 
@@ -39,6 +45,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    flash.notice = "Your article '#{@article.title}' has been updated!"
     redirect_to article_path(@article)
   end
 end
