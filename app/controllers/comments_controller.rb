caf02be2f086c2fd0d9e68a7ae78bca45a139ec5
ashwiniraokarai <ashwini@biggerpockets.com>
2020-08-28 22:58:06 -0600

    redirect to article show page after posting comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,13 @@ class CommentsController < ApplicationController
   include CommentsHelper
 
   def create
+    @article_id = params[:article_id]
     @comment = Comment.new(comment_params)
+    @comment.article_id = @article_id
     @comment.save
-    render plain: "hello"
-    # render the article show page (which is set up to show posted comments anyway)
+    # render plain: "hello"  #when testing
+
+    # redirect_to article show page after posting comment (which is already set up to show posted comments anyway)
+    redirect_to article_path(@comment.article_id)
   end
 end
