31ad26027a7244ac256e66da563aae4d58ad5d93
ashwiniraokarai <ashwini@biggerpockets.com>
2020-08-30 01:37:15 -0600

    flash success or fail status when posting comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,13 @@ class CommentsController < ApplicationController
     @comment.save
     # render plain: "hello"  #when testing
 
-    # redirect_to article show page after posting comment (which is already set up to show posted comments anyway)
-    redirect_to article_path(@comment.article_id)
+    if @comment.errors.any?
+     flash.alert = "Oops there was a problem posting your comment! Please correct errors and try again."
+     render "articles/show", locals: {:@article => @comment.article}
+    else
+     flash.notice = "Your comment was posted successfully!"
+     # redirect_to article show page after posting comment (which is already set up to show posted comments anyway)
+     redirect_to article_path(@comment.article_id)
+    end
   end
 end
